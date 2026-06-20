05a59f28e8d8793a7a9aa5b4cc260f2d86e24971
Kotaro Kuroda <kotaro.kuroda@gaxi.co.jp>
2026-06-20 23:13:45 +0900

    sandboxを作成

--- .devcontainer/script/post_attach_command.sh
+++ .devcontainer/script/post_attach_command.sh
@@ -7,6 +7,6 @@ export DEBIAN_FRONTEND=noninteractive
 SCRIPT_DIRNAME=$(cd $(dirname ${0}) && pwd)
 PROJECT_DIRNAME=$(cd ${SCRIPT_DIRNAME}/../.. && pwd)
 
-cd ${PROJECT_DIRNAME}/application
+cd ${PROJECT_DIRNAME}/sandbox
 mise install
 mise exec -- npm install
