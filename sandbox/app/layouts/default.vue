ceb8b0e06b6da0f511721fac34aaa95da02fcf9d
zen96k <jolly96k@gmail.com>
2026-06-23 02:59:03 +0900

    Update default.vue

--- sandbox/app/layouts/default.vue
+++ sandbox/app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <header class="border-b border-gray-200 dark:border-gray-800">
       <UContainer class="flex items-center justify-between py-3">
-        <span class="text-2xl font-semibold">Sandbox</span>
+        <NuxtLink to="/" class="text-2xl font-semibold">Sandbox</NuxtLink>
         <UColorModeButton />
       </UContainer>
     </header>
